e6887fca43d457a88256d154bd1fce7af7577da1
taep823 <apetty3@cps.edu>
2024-11-18 17:23:31 +0000

    v3

--- script.js
+++ script.js
@@ -19,16 +19,20 @@ let choiceA2image = document.querySelector('.choiceA2image');
 let h2 = document.querySelector('.run');
 run.style.display="none";
 
-let knife=document.querySelector('.knife');
-knife.display="none";
+let knifeMan=document.querySelector('.knifeMan');
+knifeMan.style.display="none";
+
+let choiceB1image=document.querySelector('.choiceB1image');
+choiceB1image.style.display="none";
 
 headButton.addEventListener('click', function() {
-text.innerHTML = "You find one of your friends unconscious on the operating bed. You can double click to go check on them, or search the page to run away.";
+text.innerHTML = "You find one of your friends unconscious on the operating bed. <strong>You can double click</strong> to go check on them, or <strong>search the page</strong> to run away.";
 start.style.display = "none";
 choiceAimage.style.display ="block";
 exploreButton.style.display="none";
 headButton.style.display="none";
 run.style.display="block";
+choiceB1image.style.display="none";
 });
 
 choiceAimage.addEventListener('dblclick', function(){
@@ -49,8 +53,12 @@ run.addEventListener("mouseenter", function(){
 
 exploreButton.addEventListener("click", function(){
     
-    text.innerHTML = "There’s a man down the hallway with a knife"
+    text.innerHTML = "There’s a man down the hallway with a knife, <strong>press the arrow key up</strong> to run away, or <strong>press the arrow key down</strong> to grab a scalpel on the floor"
     start.style.display = "none";
     exploreButton.style.display="none";
     headButton.style.display="none";
+    knifeMan.style.display="block";
+});
+knifeMan.addEventListener("keyup" , function() {
+    text.innerHTML = "You run and luckily find the exit and get away quickly"
 });
